1fe86186cb473133f98a1c64bfc967143510a2b5
Timmyfb <37824773+Timmyfb@users.noreply.github.com>
2018-04-16 16:43:38 +1000

    Clear Battle Function

--- Server/gameEntities/src/main/java/gameEntities/Battle.java
+++ Server/gameEntities/src/main/java/gameEntities/Battle.java
@@ -10,34 +10,7 @@ public class Battle {
 
     public Character Begin(Character c1, Character c2) {
 
-        int hp1 = c1.GetConstitution() * 10;
-        int hp2 = c2.GetConstitution() * 10;
-        int s1 = c1.GetStrength();
-        int s2 = c2.GetStrength();
-        int d1 = c1.GetDexterity();
-        int d2 = c2.GetDexterity();
-        int i1 = c1.GetIntelligence();
-        int i2 = c2.GetIntelligence();
-
-        while(hp1 > 0 || hp2>0) {
-            Random r = new Random();
-            int dice1 = r.nextInt(20);
-            int dice2 = r.nextInt(20);
-            if (d1 + dice1 > d2 + dice2) {
-                hp2 -= s1 > i1 ? s1 : i1;
-            } else if (d1 + dice1 < d2 + dice2) {
-                hp1 -= s2 > i2 ? s2 : i2;
-            } else {
-                continue;
-            }
-        }
-
-        if (hp1 > 0) {
-            return c1;
-        }
-        else {
-            return c2;
-        }
+        return c1;
 
     }
 
