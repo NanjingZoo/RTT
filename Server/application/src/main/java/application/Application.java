e5082660d81f312ae271de1ea50ada06cbebcdd6
Timmyfb <37824773+Timmyfb@users.noreply.github.com>
2018-04-26 16:41:06 +1000

    Added rest sample module

--- Server/application/src/main/java/application/Application.java
+++ Server/application/src/main/java/application/Application.java
@@ -1,33 +1,29 @@
 package application;
 
-import domain.character.*;
-import domain.character.Character;
-import domain.map.MapLocationRepository;
-import domain.map.MapRepository;
-import domain.tendency.TendencyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import service.Service;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EntityScan({"domain.character", "domain.map", "domain.tendency"})
 @EnableJpaRepositories({"domain.character", "domain.map", "domain.tendency"})
+@EnableWebMvc
+@Import(rest.GreetingController.class)
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
+		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public CommandLineRunner demo(CharacterRepository characterRepository,
 								  MapRepository mapRepository,
 								  MapLocationRepository mapLocationRepository,
@@ -46,6 +42,6 @@ public class Application {
 			service.Test();
 
 		};
-	}
+	}*/
 
 }
